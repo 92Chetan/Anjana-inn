c45f795b6ddf6437e8137b7edfb1e6d95732bbeb
kingsuk <kingsuk055@gmail.com>
2024-05-23 16:28:56 +0530

    some issue

--- src/lib/ImageUpload.ts
+++ src/lib/ImageUpload.ts
@@ -1,28 +1,16 @@
-import { mkdir, stat, writeFile } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
 import { join } from 'path';
 import mime from 'mime';
 
 export const UploadImage = async (image: File) => {
   try {
     const buffer = Buffer.from(await image.arrayBuffer());
-    const relativeUploadDir = 'uploads'; // Remove the leading slash
-    const uploadDir = join(process.cwd(), relativeUploadDir); // Join with cwd
-
-    try {
-      await stat(uploadDir);
-    } catch (error: any) {
-      if (error.code === 'ENOENT') {
-        // Create directory if it doesn't exist
-        await mkdir(uploadDir, { recursive: true });
-      } else {
-        console.log('Error while trying to create directory when uploading a file\n', error);
-        throw new Error('Failed to create upload directory');
-      }
-    }
+    const relativeUploadDir = 'uploads';
+    const uploadDir = join(process.cwd(), 'public', relativeUploadDir);
 
     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
     const filename = `${image.name.replace(/\.[^/.]+$/, '')}-${uniqueSuffix}.${mime.getExtension(image.type)}`;
-    await writeFile(join(uploadDir, filename), buffer); // Join with directory path
+    await writeFile(join(uploadDir, filename), buffer);
 
     const fileName = `${relativeUploadDir}/${filename}`;
     return fileName;
